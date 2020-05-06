75814149c450865f58f1229848fbf4320a2a1517
nyg551 <yairgre@gmail.com>
2020-05-06 11:07:58 +0300

    solution for excercise6

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+# arguments: $1 (file1) , $2 (file2), $3(to)
+
+#this line will find the last argument (path):
+for i in $@; do :; done
+
+#copy all other arguments to path
+for j in {1..i-1}
+do
+   cp "$j" a@server2.fi:$i
+   sum_sizes = sum_sizes + $(wc -c "$j);
+done
+echo sum_sizes;
