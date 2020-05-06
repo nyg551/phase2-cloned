8d4bf2fb0096d2ecef07e716491b4c7c64edd1cd
nyg551 <yairgre@gmail.com>
2020-05-07 01:54:51 +0300

    managed to find the solution @ stackoverflow

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 #add fix to exercise3 here
 ####did not succeed
-####   I added "Require all granted" to /etc/apache2/sites-enabled
+## managed to find the solution at:
+## https://stackoverflow.com/questions/18447454/apache-giving-403-forbidden-errors
 
-####  I searched for the solution but unfortunately this did not work me
+echo "<Directory />" >> /etc/hosts/httpd.conf
+echo    "Allow from all" >> /etc/hosts/httpd.conf
+echo     "Order allow,deny">> /etc/hosts/httpd.conf
+echo "</Directory>">> /etc/hosts/httpd.conf
+
+echo "<VirtualHost *:80>">> /etc/hosts/httpd.conf
+echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/hosts/httpd.conf
+echo     "ServerName mysite">> /etc/hosts/httpd.conf
+echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/hosts/httpd.conf
+echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/hosts/httpd.conf
+echo        "AllowOverride All">> /etc/hosts/httpd.conf
+echo        "Order allow,deny">> /etc/hosts/httpd.conf
+echo        "Allow from all">> /etc/hosts/httpd.conf
+echo        "Require all granted">> /etc/hosts/httpd.conf
+echo    "</Directory>">> /etc/hosts/httpd.conf
+echo "</VirtualHost>">> /etc/hosts/httpd.conf
+
