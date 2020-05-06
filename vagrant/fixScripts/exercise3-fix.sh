6034ae2f522c38fd76d39c4523c8d9c8fcf616a8
nyg551 <yairgre@gmail.com>
2020-05-06 10:09:18 +0300

    updated exercised3-fix

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+####did not succeed
+####   I added "Require all granted" to /etc/apache2/sites-enabled
+
+####  I searched for the solution but unfortunately this did not work me
