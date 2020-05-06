cbda3c9308760b3db5eb0faa2295ef9f84e23737
nyg551 <yairgre@gmail.com>
2020-05-06 10:39:29 +0300

    added solution for exercise6

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+#the solution was based on: https://linuxize.com/post/how-to-setup-passwordless-ssh-login/
+
+ssh-keygen -t rsa -b 4096 -C "a@server1"
+ssh-copy-id a@server1
+ssh a@server1
