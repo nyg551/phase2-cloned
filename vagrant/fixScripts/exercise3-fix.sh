1e319a213aa843a49bb279d78f6b52aaf1fbec8e
nyg551 <yairgre@gmail.com>
2020-05-06 11:24:44 +0300

    added more information

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #add fix to exercise3 here
 ####did not succeed
+## I tried the solution from:
+#  https://stackoverflow.com/questions/21551840/forbidden-you-dont-have-permission-to-access-on-this-server
 ####   I added "Require all granted" to /etc/apache2/sites-enabled
-
-####  I searched for the solution but unfortunately this did not work me
+###    Unfortunately - it did not work for me. I would be happy to get any tip regarding this excercise.
