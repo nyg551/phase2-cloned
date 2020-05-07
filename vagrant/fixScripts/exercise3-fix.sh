6b73c940bd6c2a1aca30d56c946a34a4a605a53e
nyg551 <yairgre@gmail.com>
2020-05-07 13:59:33 +0300

    Update exercise3

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 #add fix to exercise3 here
 ####did not succeed
-## I tried the solution from:
-#  https://stackoverflow.com/questions/21551840/forbidden-you-dont-have-permission-to-access-on-this-server
-####   I added "Require all granted" to /etc/apache2/sites-enabled
-###    Unfortunately - it did not work for me. I would be happy to get any tip regarding this excercise.
+## managed to find the solution at:
+## https://stackoverflow.com/questions/18447454/apache-giving-403-forbidden-errors
+
+echo "<Directory />" >> /etc/hosts/httpd.conf
+echo    "Allow from all" >> /etc/hosts/httpd.conf
+echo     "Order allow,deny">> /etc/hosts/httpd.conf
+echo "</Directory>">> /etc/hosts/httpd.conf
+
+echo "<VirtualHost *:80>">> /etc/hosts/httpd.conf
+echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/hosts/httpd.conf
+echo     "ServerName mysite">> /etc/hosts/httpd.conf
+echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/hosts/httpd.conf
+echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/hosts/httpd.conf
+echo        "AllowOverride All">> /etc/hosts/httpd.conf
+echo        "Order allow,deny">> /etc/hosts/httpd.conf
+echo        "Allow from all">> /etc/hosts/httpd.conf
+echo        "Require all granted">> /etc/hosts/httpd.conf
+echo    "</Directory>">> /etc/hosts/httpd.conf
+echo "</VirtualHost>">> /etc/hosts/httpd.conf
