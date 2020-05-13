6d685e25337a16c64d114d0a93f4513e50f1374d
nyg551 <yairgre@gmail.com>
2020-05-13 23:57:58 +0300

    update apache2\httpd.conf path

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -4,19 +4,19 @@
 ## managed to find the solution at:
 ## https://stackoverflow.com/questions/18447454/apache-giving-403-forbidden-errors
 
-echo "<Directory />" >> /etc/hosts/httpd.conf
-echo    "Allow from all" >> /etc/hosts/httpd.conf
-echo     "Order allow,deny">> /etc/hosts/httpd.conf
-echo "</Directory>">> /etc/hosts/httpd.conf
+echo "<Directory />" >> /etc/apache2/httpd.conf
+echo    "Allow from all" >> /etc/apache2/httpd.conf
+echo     "Order allow,deny">> /etc/apache2/httpd.conf
+echo "</Directory>">> /etc/apache2/httpd.conf
 
-echo "<VirtualHost *:80>">> /etc/hosts/httpd.conf
-echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/hosts/httpd.conf
-echo     "ServerName mysite">> /etc/hosts/httpd.conf
-echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/hosts/httpd.conf
-echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/hosts/httpd.conf
-echo        "AllowOverride All">> /etc/hosts/httpd.conf
-echo        "Order allow,deny">> /etc/hosts/httpd.conf
-echo        "Allow from all">> /etc/hosts/httpd.conf
-echo        "Require all granted">> /etc/hosts/httpd.conf
-echo    "</Directory>">> /etc/hosts/httpd.conf
-echo  "</VirtualHost>">> /etc/hosts/httpd.conf
+echo "<VirtualHost *:80>">> /etc/apache2/httpd.conf
+echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/apache2/httpd.conf
+echo     "ServerName mysite">> /etc/apache2/httpd.conf
+echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/apache2/httpd.conf
+echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/apache2/httpd.conf
+echo        "AllowOverride All">> /etc/apache2/httpd.conf
+echo        "Order allow,deny">> /etc/hoapache2sts/httpd.conf
+echo        "Allow from all">> /etc/apache2/httpd.conf
+echo        "Require all granted">> /etc/apache2/httpd.conf
+echo    "</Directory>">> /etc/apache2/httpd.conf
+echo  "</VirtualHost>">> /etc/apache2/httpd.conf
