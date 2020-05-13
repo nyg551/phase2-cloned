8b233bbf3f901c5ae5b9e9221fefb91ea6d1307b
nyg551 <yairgre@gmail.com>
2020-05-14 00:20:39 +0300

    added "sudo" to command line

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -4,19 +4,20 @@
 ## managed to find the solution at:
 ## https://stackoverflow.com/questions/18447454/apache-giving-403-forbidden-errors
 
-echo "<Directory />" >> /etc/apache2/httpd.conf
-echo    "Allow from all" >> /etc/apache2/httpd.conf
-echo     "Order allow,deny">> /etc/apache2/httpd.conf
-echo "</Directory>">> /etc/apache2/httpd.conf
 
-echo "<VirtualHost *:80>">> /etc/apache2/httpd.conf
-echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/apache2/httpd.conf
-echo     "ServerName mysite">> /etc/apache2/httpd.conf
-echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/apache2/httpd.conf
-echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/apache2/httpd.conf
-echo        "AllowOverride All">> /etc/apache2/httpd.conf
-echo        "Order allow,deny">> /etc/apache2/httpd.conf
-echo        "Allow from all">> /etc/apache2/httpd.conf
-echo        "Require all granted">> /etc/apache2/httpd.conf
-echo    "</Directory>">> /etc/apache2/httpd.conf
-echo  "</VirtualHost>">> /etc/apache2/httpd.conf
+sudo sh -c 'echo "<Directory />" >> /etc/apache2/httpd.conf'
+sudo sh -c 'echo    "Allow from all" >> /etc/apache2/httpd.conf'
+sudo sh -c 'echo     "Order allow,deny">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo "</Directory>">> /etc/apache2/httpd.conf'
+
+sudo sh -c 'echo "<VirtualHost *:80>">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo    "DocumentRoot "/Users/myusername/Sites/mySite/public"">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo     "ServerName mysite">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo    "<Directory "/Users/myusername/Sites/mySite/public">">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo        "Options Indexes FollowSymLinks Includes ExecCGI">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo        "AllowOverride All">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo        "Order allow,deny">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo        "Allow from all">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo        "Require all granted">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo    "</Directory>">> /etc/apache2/httpd.conf'
+sudo sh -c 'echo  "</VirtualHost>">> /etc/apache2/httpd.conf'
